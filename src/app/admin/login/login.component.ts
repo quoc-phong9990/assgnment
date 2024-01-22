a4282d9d628e400a21812fbd5b2d0a8b18a0fb7a
quoc-phong9990 <139205778+quoc-phong9990@users.noreply.github.com>
2024-01-22 09:30:00 +0700

    #3

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -13,20 +13,22 @@ import { Router } from '@angular/router';
 export class LoginComponent {
     loginform = new FormGroup({
         username: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.required),
         password: new FormControl('', [Validators.required,Validators.minLength(6)])
     });
     mess = '';  
     router = inject(Router);  
     onSubmit(){
        let u = this.loginform.controls.username.value;
-       let p = this.loginform.controls.password.value
-       if ((u=='ok')&&(p=='123456'))  {
+       let p = this.loginform.controls.password.value;
+       let e=this.loginform.controls.email.value;
+       if ((u=='ok')&&(p=='123456')&&(e=='phongnqph29928@fpt.edu.vn'))  {
         this.mess = "Đăng nhập thành công";
         localStorage.setItem('login','ok');
         this.router.navigate(['dashboard']);
        }
        else {
-        this.mess = "Sai tên đăng nhập hoặc mật khẩu";
+        this.mess = "Sai tên đăng nhập,enail hoặc mật khẩu";
        }
     }
 }
